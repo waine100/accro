35e842dc34a167e45f85a8d454ddb79a436271d6
flavien.chantelot <flavien.chantelot@fiducial.net>
2014-01-23 17:16:55 +0100

    First commit

---
+++ src/Zenweb/Accro/UserBundle/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+namespace Zenweb\Accro\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * Description
+ *
+ * @category Zenweb
+ * @package  Zenweb_Accro
+ *
+ * @author   Flavien Chantelot <flavien@zenweb.fr>
+ */
+class User extends BaseUser
+{
+    /**
+     * Constructor
+     *
+     * @return User
+     *
+     * @author  Flavien Chantelot <flavien@zenweb.fr>
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        /**
+         * TODO
+         */
+    }
+}
---
+++ src/Zenweb/Accro/UserBundle/ZenwebAccroUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Zenweb\Accro\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZenwebAccroUserBundle extends Bundle
+{
+}
