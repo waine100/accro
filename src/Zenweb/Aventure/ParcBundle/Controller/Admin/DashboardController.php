f178c661598aaf7ab4127dc8479c13b9fd6ef568
Mathieu <mathieu.poulenard@gmail.com>
2014-06-04 00:49:03 +0200

    start of dashboard
    to be continued

---
+++ src/Zenweb/Aventure/ParcBundle/Controller/Admin/DashboardController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Zenweb\Aventure\ParcBundle\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DashboardController extends Controller
+{
+    public function listAction()
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $parcs =  $manager->getRepository('ZenwebAventureParcBundle:Parc')->findAll();
+        $activities =  $manager->getRepository('ZenwebAventureParcBundle:Activity')->findAll();
+        return $this->render('ZenwebAventureParcBundle:Dashboard:homepage.html.twig',
+            array(
+                'admin_pool' => $this->get('sonata.admin.pool'),
+                'date' => date("d/m/Y"),
+                'parcs' => $parcs,
+                'activities' => $activities
+            )
+        );
+    }
+
+    public function contentAction($parc='',$date='',$activity='')
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $dateTime = new \DateTime(implode('-',array_reverse(explode('/',$date))));
+        $booking =  $manager->getRepository('ZenwebAventureParcBundle:Booking')->findOneBy(array('theDate' => $dateTime, 'parc' => $parc));
+        return $this->render('ZenwebAventureParcBundle:Dashboard:content.html.twig',
+            array(
+                'typicalDay' => $booking->getTypicalDay(),
+                'timeSlots'  => $booking->getTypicalDay()->getTimeSlots()
+            )
+        );
+    }
+}
