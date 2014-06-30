fbaff75fbbfc1451f654c494866a0323cecc9876
flavien.chantelot <flavien.chantelot@fiducial.net>
2014-06-30 18:51:20 +0200

    Work on quantity should be ok now

--- src/Zenweb/Aventure/ParcBundle/Validator/Constraints/ActivityQuantityValidator.php
+++ src/Zenweb/Aventure/ParcBundle/Validator/Constraints/ActivityQuantityValidator.php
@@ -12,53 +12,55 @@ use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Doctrine\ORM\EntityManager;
 
-class ActivityQuantityValidator extends ConstraintValidator {
+class ActivityQuantityValidator extends ConstraintValidator
+{
 
     private $em;
 
     public function __construct(EntityManager $em)
     {
-        $this->em      = $em;
+        $this->em = $em;
     }
 
-    public function validate($item, Constraint $constraint)
+    public function validate($order, Constraint $constraint)
     {
-        $qtyWanted = $item->getQty();
-        $qtyMax = $item->getTimeSlot()->getActivity()->getCapacity();
-        $qtyMin = $item->getTimeSlot()->getActivity()->getQtyMin();
-        $tsId = $item->getTimeSlot()->getId();
+        $date = $order->getBookingDate();
 
-        $activity = $item->getTimeSlot()->getActivity();
-        $timeSlot = $item->getTimeSlot();
+        $placesWanted = array();
 
-        /**
-         * @todo
-         * LA DATE EST PAS BONNE. IL FAUT STOCKER LA DATE QUELQUEPART ABSOLUMENT
-         * AUTRE PB n'est pas bon si plusieurs items sur le même timeslot
-         * AUTRE PB Me souviens plus mais il y a un truc.
-         */
-        $date = $item->getTimeSlot()->getBeginTime();
+        foreach ($order->getItems() as $item) {
+            $qtyWanted = $item->getQty();
+            $qtyMax    = $item->getTimeSlot()->getCapacity();
+            $qtyMin    = $item->getTimeSlot()->getActivity()->getQtyMin();
+            $tsId      = $item->getTimeSlot()->getId();
 
-        if($qtyWanted > $qtyMax) {
-            $this->context->addViolation($constraint->tooMuchMessage, array('{{ max }}' => $qtyMax, '{{ timeSlot }}' => $timeSlot, '{{ activity }}' => $activity));
-            return;
+            $activity = $item->getTimeSlot()->getActivity();
+            $timeSlot = $item->getTimeSlot();
+
+            if (isset($placesWanted[$tsId])) {
+                $placesWanted[$tsId] += $qtyWanted;
+            } else {
+                $placesWanted[$tsId] = $qtyWanted;
+            }
+
+
+            if ($qtyWanted > $qtyMax) {
+                $this->context->addViolation($constraint->tooMuchMessage, array('{{ max }}' => $qtyMax, '{{ timeSlot }}' => $timeSlot, '{{ activity }}' => $activity));
+            }
+
+            if ($qtyWanted < $qtyMin) {
+                $this->context->addViolation($constraint->tooLessMessage, array('{{ min }}' => $qtyMin, '{{ timeSlot }}' => $timeSlot, '{{ activity }}' => $activity));
+            }
+
+            /**
+             * Check how many places we have left
+             */
+            $placesReserved = $this->em->getRepository('ZenwebAventureParcBundle:SalesFlatOrder')->getPlacesReserved($date, $tsId);
+            $placesLeft     = $qtyMax - $placesReserved - $placesWanted[$tsId];
+
+            if ($placesLeft < 0) {
+                $this->context->addViolation($constraint->message, array('{{ limit }}' => $placesLeft, '{{ timeSlot }}' => $timeSlot, '{{ activity }}' => $activity));
+            }
         }
-
-        if($qtyWanted < $qtyMin) {
-            $this->context->addViolation($constraint->tooLessMessage, array('{{ min }}' => $qtyMin, '{{ timeSlot }}' => $timeSlot, '{{ activity }}' => $activity));
-            return;
-        }
-
-        /**
-         * Check how many places we have left
-         */
-        $placesReserved = $this->em->getRepository('ZenwebAventureParcBundle:SalesFlatOrder')->getPlacesReserved($date, $tsId);
-        $placesLeft = $qtyMax - $placesReserved;
-
-        if($placesLeft < 0) {
-            $this->context->addViolation($constraint->message, array('{{ limit }}' => $placesLeft, '{{ timeSlot }}' => $timeSlot, '{{ activity }}' => $activity));
-            return;
-        }
-
     }
 } 
